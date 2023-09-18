ea938e9778619a13ac95abed9f7931ece824a6c6
ErickSalazar44 <erickearl22@gmail.com>
2023-09-18 13:50:39 -0500

    'alt img'

--- src/components/Home/MovieBackground.jsx
+++ src/components/Home/MovieBackground.jsx
@@ -48,7 +48,7 @@ const MovieBackground = ({
                                 ? backgroundImage
                                 : "/noImage.avif"
                         }
-                        alt={movies?.[currentIndex]?.title}
+                        alt={movies?.[currentIndex].original_name}
                         className='absolute z-[1] top-0 w-[100vw] right-0 h-[85vh] min-w-full object-cover saturate-[1.2]'
                         onLoad={(e) => {
                             e.target.style.opacity = 1;
